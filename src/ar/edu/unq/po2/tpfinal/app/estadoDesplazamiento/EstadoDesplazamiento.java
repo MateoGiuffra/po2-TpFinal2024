fd54c4b05c3d001a4be93d62229c4022baba0426
MateoGiuffra <matteogiuffrah40@gmail.com>
2024-06-10 21:06:26 -0300

    commit de prueba

--- src/ar/edu/unq/po2/tpfinal/app/estadoDesplazamiento/EstadoDesplazamiento.java
+++ src/ar/edu/unq/po2/tpfinal/app/estadoDesplazamiento/EstadoDesplazamiento.java
@@ -5,4 +5,5 @@ import ar.edu.unq.po2.tpfinal.app.AppUsuario;
 public abstract class EstadoDesplazamiento {
 	public abstract void manejando(AppUsuario app);
 	public abstract void caminando(AppUsuario app);
+	
 }
